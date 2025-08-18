d0cb41047f363acf3829d89c2567a27acaa5c563
Vishal Bagal <bagal9464@gmail.com>
2025-08-18 09:28:27 +0530

    Update polynomial_regression.py

--- Machine_learning/Supervised_Learning/Regression/polynomial_regression.py
+++ Machine_learning/Supervised_Learning/Regression/polynomial_regression.py
@@ -23,7 +23,6 @@ lin_model_pred = lin_reg.predict([[6]])
 lin_model_pred
 
 #polinomial regression model
-
 from sklearn.preprocessing  import PolynomialFeatures
 poly_reg = PolynomialFeatures(degree = 5)
 X_poly = poly_reg.fit_transform(X)
@@ -34,7 +33,6 @@ lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
 
 #poly nomial visualization
-
 plt.scatter(X,y, color = 'red')
 plt.plot(X,lin_reg_2.predict(poly_reg.fit_transform(X)),color = 'blue')
 plt.title('polymodel (polymodel Regression)')
@@ -44,6 +42,7 @@ plt.show()
 
 poly_model_pred = lin_reg_2.predict(poly_reg.fit_transform([[7]]))
 poly_model_pred
+
 
 
 
